ff6dd0dc2148b81512cc41497b9b7b3dfe9a3fcf
vagrant <zuriel.wz@gmail.com>
2015-08-02 23:55:01 +0000

    Editing page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, :only => [:new, :create]
+ before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 def index
 	@places = Place.all
 	@places =  Place.paginate(:page => params[:page], :per_page => 10 )
@@ -20,11 +20,17 @@ end
 
  def edit
    @place = Place.find(params[:id])
+   if @place.user != current_user
+    return render :text => 'Not Allowed', :status => :forbidden
+  end
 end
 
 def update
 end
  @place = Place.find(params[:id])
+ if @place.user != current_user
+    return render :text => 'Not Allowed', :status => :forbidden
+  end
   @place.update_attributes(place_params)
    redirect_to root_path
 
