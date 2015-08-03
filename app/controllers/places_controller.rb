29613c61c2db64f5b96eba542e768d36381aff27
vagrant <zuriel.wz@gmail.com>
2015-08-03 00:43:55 +0000

    Validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ def new
 end
 
  def create
-   current_user.places.create(place_params)
+   @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, :status => :unprocessable_entity
+  end
   end
 
  def show
@@ -32,7 +36,11 @@ end
     return render :text => 'Not Allowed', :status => :forbidden
   end
   @place.update_attributes(place_params)
-   redirect_to root_path
+    if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, :status => :unprocessable_entity
+  end
 
 def destroy
   @place = Place.find(params[:id])
