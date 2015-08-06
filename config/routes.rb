ccaa1c1aadd0b5406d9af1090cffd584cd4e285a
vagrant <zuriel.wz@gmail.com>
2015-08-06 01:00:17 +0000

    Sbmit button for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-resources :places
+   resources :places do
+    resources :comments, :only => :create
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
