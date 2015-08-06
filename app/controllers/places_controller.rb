8390a81af980533973cfa657ad49b3b96785c967
vagrant <zuriel.wz@gmail.com>
2015-08-06 00:41:14 +0000

    form to each place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,7 @@ class PlacesController < ApplicationController
 def index
 	@places = Place.all
 	@places =  Place.paginate(:page => params[:page], :per_page => 10 )
+ 
   end
 
 def new
@@ -20,6 +21,7 @@ end
 
  def show
    @place = Place.find(params[:id])
+    @comment = Comment.new
   end
 
  def edit
