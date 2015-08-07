919707b5d8bce2a4ebb79ae586fd1597532c1508
vagrant <zuriel.wz@gmail.com>
2015-08-07 22:34:13 +0000

    Testing

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+
 def index
-	@places = Place.all
 	@places =  Place.paginate(:page => params[:page], :per_page => 10 )
  
   end
